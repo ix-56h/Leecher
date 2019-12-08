608c8211e37a77cf54915dacf6c0b42179327c31
Phalor <niguinti@e1r12p4.42.fr>
2019-12-08 15:14:24 +0100

    Sublist3r implemented

--- leecher.py
+++ leecher.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
+import sys
+sys.path.append('Sublist3r')
+
 import argparse, requests
+import sublist3r
+from art import *
 
 basic_tests =   [
                 ".htaccess",
@@ -15,28 +20,46 @@ basic_tests =   [
 
 class   Leecher:
     def __init__(self):
+        ascii_header = text2art("LEECHER", "rand")
+        print(ascii_header)
+        print("\t\t\tAutomated Security Audit\n")
         parser = argparse.ArgumentParser()
         parser.add_argument("-t", "--target", help="Set the target") 
         parser.add_argument("--full-check", help="Make a recursive check for all subdomains for all CMS", action="store_true")
-        args = parser.parse_args()
-        if not args.target:
+        self.args = parser.parse_args()
+        if not self.args.target:
             parser.error("Target needed")
-    
-    def sublister_wrapper(self):
-        #use sublister and save urls with list
+
+    def launch_scan(self):
+        #self.check_domain(self, args.target)
+        self.sublister_wrapper(self.args.target)
+        return
+
+    def sublister_wrapper(self, domain):
+        print("Launching Sublist3r...")
+        subdomains = sublist3r.main(
+                    domain,
+                    40,
+                    None,
+                    ports=None,
+                    silent=True, verbose=False, enable_bruteforce=False, engines=None
+                    )
+        print(subdomains)
         return
 
     def check_domain(self, domain_url):
+        print("Check server status...")
         #if fail to connect go to next 
         #else check basic_tests and check if CMS are used and check versionning
         return
 
     def wpscan_wrapper(self):
+        print("WPScan processing...")
         #use wpscan and save result with sublist for the current url
         return
 
 def     leecher_launch():
-    instance = Leecher()    
-
+    instance = Leecher()
+    instance.launch_scan()
 if __name__ == '__main__':
     leecher_launch()
